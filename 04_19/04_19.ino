3b4eb326a8afa5293ed9daecf169d2daf15a6075
grig <prowlergrig@yahoo.ie>
2023-10-13 18:23:46 +0300

    Update 04_19.ino

--- 04_19/04_19.ino
+++ 04_19/04_19.ino
@@ -1,4 +1,4 @@
-// OSC out: 04 - distanta WC, 19 - vibratie carpeta 2
+// OSC out: 04 - distanta WC, 19 - accel carpeta 2
 
 #include "WiFiConfig.h"
 #include <ESP8266WiFi.h>
@@ -7,19 +7,24 @@
 #include <OSCMessage.h>
 #include <OSCBundle.h>
 #include <OSCData.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
 
 WiFiUDP Udp;                                // A UDP instance to let us send and receive packets over UDP
 OSCErrorCode error;
 const unsigned int outPort = 9904;          // remote port to receive OSC
 const unsigned int localPort = 8804;        // local port to listen for OSC packets
 
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
 const int TRIG_PIN = 15;       // D8
 const int ECHO_PIN = 13;       // D7
-const int vib1 = 14;     // D5
-const int vib2 = 5;      // D1
-
-unsigned int vib1state = LOW;
-unsigned int vib2state = LOW;
+//const int vib1 = 14;     // D5
+//const int vib2 = 5;      // D1
+//
+//unsigned int vib1state = LOW;
+//unsigned int vib2state = LOW;
 
 long duration;
 int distance;
@@ -53,21 +58,35 @@ void setup() {
 #endif
 
   delay(500);
+
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+  accel.setRange(ADXL345_RANGE_2_G);
   
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-  pinMode(vib1, INPUT);
-  pinMode(vib2, INPUT);
+//  pinMode(vib1, INPUT);
+//  pinMode(vib2, INPUT);
 }
 
 void loop() {
   OSCBundle bundle;
   
   // send data
-  vib1state = digitalRead(vib1);
-  Serial.print(vib1state);
-  vib2state = digitalRead(vib2);
-  Serial.println(vib2state);
+//  vib1state = digitalRead(vib1);
+//  Serial.print(vib1state);
+//  vib2state = digitalRead(vib2);
+//  Serial.println(vib2state);
+
+  sensors_event_t event; 
+  accel.getEvent(&event);
+  Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.println(event.acceleration.z);
   
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
@@ -80,7 +99,8 @@ void loop() {
 
   bundle.empty();
   bundle.add("/04").add(distance);
-  bundle.add("/19").add(vib1state).add(vib2state);
+//  bundle.add("/19").add(vib1state).add(vib2state);
+  bundle.add("/19").add(event.acceleration.x).add(event.acceleration.y).add(event.acceleration.z);
   Udp.beginPacket(host_ip, outPort);
   bundle.send(Udp);
   Udp.endPacket();
