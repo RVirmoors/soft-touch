795e5137e194432e9621c83460bbb528c7c5d095
grig <prowlergrig@yahoo.ie>
2023-10-20 10:26:55 +0300

    valiza neopixel

--- 16_22/16_22.ino
+++ 16_22/16_22.ino
@@ -8,6 +8,8 @@
 #include <OSCBundle.h>
 #include <OSCData.h>
 
+#include <Adafruit_NeoPixel.h>
+
 WiFiUDP Udp;                                // A UDP instance to let us send and receive packets over UDP
 OSCErrorCode error;
 const unsigned int outPort = 9916;          // remote port to receive OSC
@@ -15,6 +17,10 @@ const unsigned int localPort = 8816;        // local port to listen for OSC pack
 
 const int vib1 = 14;     // D5
 const int valizaPin = 12;  // D6
+const int ledPin = 13;   // D7
+
+#define NUM_PIXELS     24
+Adafruit_NeoPixel WS2812B(NUM_PIXELS, 13, NEO_GRB + NEO_KHZ800);
 
 unsigned int vib1state = LOW;
 unsigned int valiza = LOW;
@@ -52,6 +58,9 @@ void setup() {
   
   pinMode(valizaPin, INPUT);
   pinMode(vib1, INPUT);
+//  pinMode(ledPin, OUTPUT);
+
+  WS2812B.begin();  // INITIALIZE WS2812B strip object (REQUIRED)
 }
 
 void loop() {
@@ -62,6 +71,22 @@ void loop() {
   Serial.print("Vib lana: ");Serial.println(vib1state);
   valiza = digitalRead(valizaPin);
   Serial.print("Valiza: ");Serial.println(valiza);
+
+
+  if (valiza) {
+    // 1 = valiza e inchisa
+    WS2812B.clear();
+    WS2812B.show();
+    Serial.println(valiza);
+  } else {
+    for (int pixel = 0; pixel < NUM_PIXELS; pixel++) {           // for each pixel
+      WS2812B.setPixelColor(pixel, WS2812B.Color(80, 60, 10));  // it only takes effect if pixels.show() is called
+      WS2812B.show();                                           // send the updated pixel colors to the WS2812B hardware.
+    }
+    WS2812B.setBrightness(200);
+    WS2812B.show();   
+  }
+
 
   bundle.empty();
   bundle.add("/16").add(valiza);
